e3985ca807feba778ce0b9900402ca51406eeda6
Oliwia <os813@nyu.edu>
2019-11-14 15:54:32 -0500

    homework

---
+++ app.js
@@ -0,0 +1,6 @@
+// make a const for express
+// const express = require('express')
+// const app = express()
+// const port procsess.env.PORT || 4000;
+
+// make routes for all of the pages
---
+++ index.js
@@ -0,0 +1,35 @@
+// this should be inside of the routes folder
+// need firebase config
+
+// const express =require('express')
+// const router = express.Router()
+// const firebase = require('firebase')
+
+// const firebaseConfig = (
+//     // copy and paste all of the stuff from firebase into here
+// )
+
+// const firestoreDatabase = firebase.initializeApp(firebaseConfig)
+
+// const db = firestoreDatabase.firestore();
+
+// need to get all of the api keys and stuff and plug in information into here
+
+// let posts =[]
+// db.collection('blog-posts').get()
+// .then(
+//     blogPosts =>(
+//         posts = blogPosts.data()
+
+//     )
+
+// )
+// .catch(err)=>(
+//     console log the error
+// )
+
+// router.get('/', (req,res)) =>{
+//     res.send(posts)
+// }
+
+// module.exports=router
